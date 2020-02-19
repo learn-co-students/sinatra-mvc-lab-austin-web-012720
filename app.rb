a7f1e5e44f1452fc0d77c110217be7bb4e49aef1
Jp1200 <jprkr96@gmail.com>
2020-02-19 15:00:26 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+
+    erb :user_input
+  end
+  post '/' do
+    @input = PigLatinizer.new(params[:user_phrase])
+    erb :view
+  end
+
+
+
 end
